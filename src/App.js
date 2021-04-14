b9694512c2524b6c01a805929c9d980926b9de5d
n1ckDotEXE <admin@nicholasnam.com>
2021-04-14 14:40:59 -0400

    Retrieves data from omdbapi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import "./App.css";
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
 	const [number, setNumber] = useState(0);
+	const [movieInput, setMovieInput] = useState("spiderman");
 
 	function handleMinusClick() {
-		console.log("Minus");
+		//setNumber(number - 1);
+		setNumber((prevValue) => prevValue - 1);
 	}
 
 	function handlePlusClick() {
-		console.log("Plus");
+		//setNumber(number + 1);
+		setNumber((prevValue) => prevValue + 1);
+	}
+
+	async function fetchMovie() {
+		const response = await fetch(
+			`http://www.omdbapi.com/?apikey=6332b1e1&s=${movieInput}`
+		);
+		const data = await response.json();
+		console.log(data);
+		try {
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	useEffect(() => {
+		console.log("useEffect ran");
+		fetchMovie();
+	}, [movieInput]);
+	function handleMovieOnChange(event) {
+		setMovieInput(event.target.value);
 	}
 
 	return (
@@ -19,7 +42,16 @@ function App() {
 				<button onClick={handleMinusClick}>-</button>
 				<button onClick={handlePlusClick}>+</button>
 			</div>
+			<div>
+				<input
+					type="text"
+					name="movieInput"
+					value={movieInput}
+					onChange={handleMovieOnChange}
+				/>
+			</div>
 		</div>
 	);
 }
+
 export default App;
